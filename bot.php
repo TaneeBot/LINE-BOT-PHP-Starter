274c77d55c5699940696c2c57dd967fb52648fee
TaneeBot <37092524+TaneeBot@users.noreply.github.com>
2018-03-16 18:44:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,6 @@
 <?php 
 echo "I am a bot";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
